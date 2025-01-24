2f3dbee54eb2b622093347c3f207019427f93eb2
vignesh141193 <vignesh.k.sekar@gmail.com>
2025-01-24 12:33:36 +0530

    Update snowconn.py

--- snowconn.py
+++ snowconn.py
@@ -2,7 +2,6 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 
-
 # Function to connect to Snowflake
 def connect_snowflake(config):
     conn = snowflake.connector.connect(
@@ -14,19 +13,12 @@ def connect_snowflake(config):
         database=config['database'],
         schema=config['schema']
     )
+    st.success("Snowflake connection successfull")
     return conn
-
-# Function to fetch Snowflake table metadata
-def get_snowflake_metadata(conn, table_name):
-    cursor = conn.cursor()
-    cursor.execute(f"DESC TABLE {table_name}")
-    metadata = cursor.fetchall()
-    cursor.close()
-    return metadata
-
-
-# Streamlit app
 def main():
+    st.title("Schema Validation App")
+
+    # Snowflake connection details
     config = {
         'account': st.text_input('Snowflake Account'),
         'user': st.text_input('Snowflake User'),
@@ -38,14 +30,7 @@ def main():
         'table': st.text_input('Snowflake Table Name')
     }
 
-    if st.button('Fetch Metadata'):
-            conn = connect_snowflake(config)
-            metadata = get_snowflake_metadata(conn, config['table'])
-            
-            # Show metadata in a table without specifying column names
-            metadata_df = pd.DataFrame(metadata)
-            st.write("Snowflake Table Metadata:")
-            st.write(metadata_df)
-
+    if st.button('connect'):
+        conn = connect_snowflake(config)
 if __name__ == "__main__":
     main()
